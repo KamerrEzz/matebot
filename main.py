a57be9971a4b7453920ffe7717408141bb1fe0c0
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-10-28 13:52:45 -0300

    Update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 import os
 
 import discord
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
